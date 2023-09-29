d4e114235ce4883a997b4b94026919b69036f0a9
Ilya Zhevlakov <ilya8bojik@gmail.com>
2023-09-29 17:49:48 +0300

    Init project

---
+++ app/build.gradle.kts
@@ -0,0 +1,46 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.kotlinAndroid)
+}
+
+android {
+    namespace = "ru.izhxx.gimodule"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "ru.izhxx.gimodule"
+        minSdk = 28
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to arrayOf("lib*.so"))))
+    implementation(libs.core.ktx)
+    implementation(libs.ezxhelper)
+
+    compileOnly(libs.xposed)
+    compileOnly(libs.xposed.sources)
+}
